f52b2396ee71aa60e2ad14a28d4840b118e7720c
Michael Ernst <mernst@cs.washington.edu>
2025-09-07 23:11:17 -0700

    lexical binding

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -1,4 +1,4 @@
-;;; early-init.el --- early bird  -*- no-byte-compile: t -*-
+;;; early-init.el  -*-  lexical-binding: t; no-byte-compile: t -*-
 (setq load-prefer-newer t)
 (add-to-list 'load-path "~/.emacs.d/elpa/auto-compile-20250531.2214/")
 (require 'auto-compile)
